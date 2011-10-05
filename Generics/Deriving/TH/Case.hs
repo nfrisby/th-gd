a838c6b4af9a103c0fbe253f10508f4bafbbbba7
Nicolas Frisby <nfrisby@ittc.ku.edu>
2011-10-05 17:06:26 -0500

    renamed to conCase; bugs

--- Generics/Deriving/TH/Case.hs
+++ Generics/Deriving/TH/Case.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE ViewPatterns, TypeOperators, TemplateHaskell #-}
 
-module Generics.Deriving.TH.Case (gd_case, varE) where
+module Generics.Deriving.TH.Case (conCase, varE) where
 
 import Language.Haskell.TH
 
 import Generics.Deriving hiding (conName)
-import Generics.Deriving.TH
 
 import qualified Data.Map as Map
-import Data.Traversable (forM)
 
+import qualified Control.Arrow as Arrow
 
 
 
@@ -20,41 +19,38 @@ import Data.Traversable (forM)
 -- default function to use as the other branch. The third is an injection
 -- function (i.e. @return@) applied to the argument and result of the second
 -- function.
-gd_case :: [Q Exp] -> (Q Exp -> Q Exp) -> Q Exp
-gd_case cases defalt = do
-  x <- sequence cases
-  gd_case' x defalt
+conCase :: Q Exp -> (Q Exp -> Q Exp) -> Q Exp
+conCase cas defalt = do
+  x <- cas
+  conCase' x defalt
 
-gd_case' (mapM open -> Just cases@((con1, _) : _)) defalt = do
+conCase' (CaseE disc (mapM open -> Just cases@((con1, _) : _))) defalt = do
   DataConI _ _ ty _ <- reify con1
   TyConI (DataD _ _ tvbs cons _) <- reify ty
-  case cons of
-    [] -> [| error $(stringE ("gd_case of consructorless datatype " ++ show ty)) |]
+  (`AppE` disc) `fmap` case cons of
+    [] -> [| error $(stringE ("conCase of consructorless datatype " ++ show ty)) |]
     cons ->
-      [| $(foldr (\x y inj -> [| $(each x [| $inj . L1 |]) ||| $(y [| $inj . R1 |]) |])
-                   (each (last cons))
+      [| $(foldr (\x y inj -> [| $(each ascribe x [| $inj . L1 |]) ||| $(y [| $inj . R1 |]) |])
+                   (each ascribe (last cons))
                    (init cons) [| id |])
-       . unM1 . $(varE 'from) . $(return ascribe) |]
+       . unM1 . $(varE 'from) . $ascribe |]
       where
-        ascribe = SigE (VarE 'id) qty where
+        ascribe = return $ SigE (VarE 'id) qty where
             qty = ForallT tvbs [] $ (ArrowT `AppT` ty' `AppT` ty')
             ty' = foldl AppT (ConT ty) $ map tvb_var tvbs
   where
     given_cons = Map.fromList cases
-    each con inj = case Map.lookup (conName con) given_cons of
-      Just ([], e) -> [| \_ -> $(return e) |]
-      Just (ps, e) ->
-        [| $(lamE [foldr (\p pat -> conP '(:*:) [eachP p, pat]) (eachP (last ps)) (init ps)]
-                    (return e)) . unM1 |]
+    each ascribe con inj = case Map.lookup (conName con) given_cons of
+      Just (Branch ps wrap, e)
+        | null ps -> return $ LamE [wrap WildP] e
+        | otherwise -> newName "x" >>= \n ->
+        lamE [AsP n `fmap` conP 'M1 [foldr (\p pat -> conP '(:*:) [eachP p, pat]) (eachP (last ps)) (init ps)]]
+               (letE [valD (wrap `fmap` wildP) (normalB [| $ascribe $ $(varE 'to) $ M1 $ $inj $(varE n) |]) []]
+                     (return e))
         where eachP p = return $ ConP 'M1 [ConP 'K1 [p]]
       Nothing -> defalt [| $(varE 'to) . M1 . $inj |]
+conCase' _ _ = fail "the first argument must be a case where each branch is a simple constructor pattern (no guards, no where clauses)"
 
-
-{-D1 T_ (C1 T_C_ (S1 NoSelector (Rec0 Int))
-       :+:
-       C1 T_D_ (:*: (S1 NoSelector (Rec0 Int)) (:*: (S1 NoSelector (Rec0 Bool)) (S1 NoSelector (Rec0 Char))))
-       :+:
-       C1 T_B_ (S1 NoSelector U1))-}
 
 
 tvb_var (PlainTV n) = VarT n
@@ -62,7 +58,18 @@ tvb_var (KindedTV n _) = VarT n
 
 
 
-open (LamE [ConP con ps] e) = Just (con, (ps, e)) where
+data Branch = Branch [Pat] (Pat -> Pat)
+
+open (Match pat (NormalB e) []) = (\(con, bra) -> (con, (bra, e))) `fmap` w pat where
+  w (ConP con ps) = Just (con, Branch ps id)
+  w (InfixP p1 con p2) = Just (con, Branch [p1, p2] id)
+--  w (RecP n fps) = (con, FieldPats fps)
+--  w (BangP pat) = under BangP $ w pat
+  w (SigP pat ty) = under (flip SigP ty) $ w pat
+  w (AsP n pat) = under (AsP n) $ w pat
+  w _ = Nothing
+
+  under f = fmap (Arrow.second $ \(Branch ps g) -> Branch ps (f . g))
 open _ = Nothing
 
 
@@ -71,6 +78,7 @@ conName (NormalC n _) = n
 conName (RecC n _) = n
 conName (InfixC _ n _) = n
 conName (ForallC _ _ con) = conName con
+
 
 
 unM (M1 x) = x
